0a50de58593e7eef9d163421bc4a8d0b30d4a4d9
sniop.safe@gmail.com <4bmnRq9p>
2021-02-28 21:24:16 +0000

    use loading bar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,15 +2,21 @@ import {getInitialData} from '../utils/api.js'
 import {getUsers} from './users.js'
 import {getTweets} from './tweets.js'
 import {setAuthedUser} from './authedUser.js'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
 
 const AUTHED_USER_ID  = 'tylermcginnis'
 export function handleInitialData(){
 
     return (dispatch) => {
+
+        dispatch(showLoading())
+
         return getInitialData().then(({users,tweets})=>{ //todo-abs why do we need to return at this step , what are we returning ?
             dispatch(getUsers(users))
             dispatch(getTweets(tweets))
             dispatch(setAuthedUser(AUTHED_USER_ID))
+            dispatch(hideLoading())
         })
     }
 }
